283526d783562bb696818e999152876e5c9ea1a9
Sam Luby <samwluby@gmail.com>
2019-05-22 19:40:56 +0100

    2d array in place. need to work on columns

--- main.py
+++ main.py
@@ -5,4 +5,26 @@ lines = f.readlines()
 
 n = 3
 lines = [line.replace('x', ' ').replace(',', '').strip('\n') for line in lines]
-[print('|'.join([line[i:i+n] for i in range(0, len(line), n)])) for line in lines]
+lines_fmtd = ['|'.join([line[i:i+n] for i in range(0, len(line), n)]) for line in lines]
+[print(line) for line in lines_fmtd]
+
+
+board = [list(line) for line in lines]
+[print(boardline) for boardline in board]
+
+# board is a 2d array
+print(board[0][3])
+
+# rows will be board[0..n][0]
+# cols will be board[0][0..n]
+
+
+
+# dont need to explicitly make rows but ok
+rows = [board[i:i+1][0] for i in range(0, len(board), 1)]
+print("ROWS: {0}".format(rows))
+
+
+# fix this it doesnt work
+cols = [board[0][i:i+1] for i in range(0, len(board), 1)]
+print("COLS: {0}".format(cols))
