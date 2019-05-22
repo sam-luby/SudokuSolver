d77b9cfa07823aa64c2623d1343c1b537f068bbc
Sam Luby <sam.luby@ammeon.com>
2019-05-22 14:36:46 +0100

    nicer printing

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-f = open("sample.txt.", "r")
+f = open("sample.txt", "r")
 lines = f.readlines()
-for line in lines:
-	print(line)
-
+# for line in lines:
+# 	print(line)
 
 n = 3
 lines = [line.replace('x', ' ').replace(',', '').strip('\n') for line in lines]
-
-[print(line) for line in lines]
-
-
+[print('|'.join([line[i:i+n] for i in range(0, len(line), n)])) for line in lines]
