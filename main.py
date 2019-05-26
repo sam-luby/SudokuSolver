4c71ed74f8ce887ae642d1bce404feea80dcca24
Sam Luby <samwluby@gmail.com>
2019-05-26 22:47:51 +0100

    added functionality to get block indexes and check missing numbers in blocks

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import numpy as np
 
-f = open("sample.txt", "r")
 RANGE = list(range(1,10))
 
 
-lines = f.readlines()
-lines = [line.replace('x', ' ').replace(',', '').strip('\n') for line in lines]
-lines_fmtd = ['|'.join([line[i:i+3] for i in range(0, len(line), 3)]) for line in lines]
-[print(line) for line in lines_fmtd]
-
-board = np.array([list(line) for line in lines])
-
-# print("Range of columns", board[:, 1:3])
-rows = [board[x] for x, row in enumerate(board, 0)]
-cols = [board[:, x] for x, col in enumerate(board, 0)]
-
+def create_board(filename):
+	f = open(filename, 'r')
+	lines = f.readlines()
+	lines = [line.replace('x', ' ').replace(',', '').strip('\n') for line in lines]
+	lines_fmtd = ['|'.join([line[i:i+3] for i in range(0, len(line), 3)]) for line in lines]
+	board = np.array([list(line) for line in lines])
+	return board
 
 # Gets the 3x3 block on the sudoku board
 #   1[0:2][0:2] | 2[0:2][3:5] | 3[0:2][6:8]
@@ -43,16 +38,68 @@ def get_block_region(row_index, column_index):
 		elif column_index <= 8:
 			return 9
 
-def get_missing_numbers(row_or_index):
+def get_block_indexes(block):
+	if block == 1:
+		return range(0,3), range(0,3)
+	if block == 2:
+		return range(0,3), range(3,6)
+	if block == 3:
+		return range(0,3), range(6,9)
+	if block == 4:
+		return range(3,6), range(0,3)
+	if block == 5:
+		return range(3,6), range(3,6)
+	if block == 6:
+		return range(3,6), range(6,9)
+	if block == 7:
+		return range(6,9), range(0,3)
+	if block == 8:
+		return range(6,9), range(3,6)
+	if block == 9:
+		return range(6,9), range(6,9)
+
+# returns a list of missing numbers in a row/column
+def get_missing_numbers_row_column(row_or_column):
 	missing_numbers = []
 	for num in RANGE:
-		if num not in [int(x) for x in row_or_index if x.isdigit()]:
+		if num not in [int(x) for x in row_or_column if x.isdigit()]:
+			missing_numbers.append(num)
+	return missing_numbers
+
+# returns a list of missing numbers in a given 3x3 block
+def get_missing_numbers_block(block, board):
+	missing_numbers = []
+	numbers_present = []
+	
+	row_indexes, column_indexes = get_block_indexes(block)
+
+	for row_index in row_indexes:
+		for column_index in column_indexes:
+			if board[row_index][column_index].isdigit():
+				numbers_present.append(board[row_index][column_index])
+	
+	for num in RANGE:
+		if num not in [int(x) for x in numbers_present if x.isdigit()]:
 			missing_numbers.append(num)
 	return missing_numbers
 
 
+
+board = create_board('sample.txt')
+print(board)
+
+# print("Range of columns", board[:, 1:3])
+rows = [board[x] for x, row in enumerate(board, 0)]
+cols = [board[:, x] for x, col in enumerate(board, 0)]
+
 for x, row in enumerate(rows, 0):
-	print("Missing numbers in row {0}:".format(x), get_missing_numbers(row))
+	print("Missing numbers in row {0}:".format(x),
+		get_missing_numbers_row_index(row))
 
 for x, col in enumerate(cols, 0):
-	print("Missing numebrs in col {0}".format(x), get_missing_numbers(col))
+	print("Missing numbers in col {0}".format(x),
+		get_missing_numbers_row_index(col))
+
+
+missing_nums = get_missing_numbers_block(5, board)
+print(missing_nums)
