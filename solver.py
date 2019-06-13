80395c099b2d6bc8a1154379b0cd06d2c98ea938
Sam Luby <samwluby@gmail.com>
2019-06-13 19:24:50 +0100

    cleanup

--- solver.py
+++ solver.py
@@ -170,32 +170,6 @@ def check_every_loc(board):
 board = create_board('sample.txt')
 print(board)
 
-# print("Range of columns", board[:, 1:3])
-rows = [board[x] for x, row in enumerate(board, 0)]
-cols = [board[:, x] for x, col in enumerate(board, 0)]
-
-
-## tests
-# for row_num in range(0,9):
-# 	print("\nMissing numbers in row {0}:".format(row_num),
-# 		get_present_missing_numbers_row(row_num, board)["missing"])
-# 	print("Present numbers in row {0}:".format(row_num),
-# 		get_present_missing_numbers_row(row_num, board)["present"])
-
-# for col_num in range(0,9):
-# 	print("\nMissing numbers in col {0}".format(col_num),
-# 		get_present_missing_numbers_column(col_num, board)["missing"])
-# 	print("Present numbers in col {0}".format(col_num),
-# 		get_present_missing_numbers_column(col_num, board)["present"])
-
-# for x in range(1, 9):
-# 	missing_nums = get_present_missing_numbers_in_block(x, board)["missing"]
-# 	print("Missing numbers in block {0}: {1}".format(x, missing_nums))
-
-
-# print(check_number_not_in_row_and_column_and_block(1, [1,1], board))
-
-# print(check_number_present_in_adjacent_rows_columns(5, [1, 1], board))
 
 while np.any(board == ' '):
 	uniques = check_every_loc(board)
---
+++ test.py
@@ -0,0 +1,25 @@
+import solver
+
+
+
+## tests
+# for row_num in range(0,9):
+# 	print("\nMissing numbers in row {0}:".format(row_num),
+# 		get_present_missing_numbers_row(row_num, board)["missing"])
+# 	print("Present numbers in row {0}:".format(row_num),
+# 		get_present_missing_numbers_row(row_num, board)["present"])
+
+# for col_num in range(0,9):
+# 	print("\nMissing numbers in col {0}".format(col_num),
+# 		get_present_missing_numbers_column(col_num, board)["missing"])
+# 	print("Present numbers in col {0}".format(col_num),
+# 		get_present_missing_numbers_column(col_num, board)["present"])
+
+# for x in range(1, 9):
+# 	missing_nums = get_present_missing_numbers_in_block(x, board)["missing"]
+# 	print("Missing numbers in block {0}: {1}".format(x, missing_nums))
+
+
+# print(check_number_not_in_row_and_column_and_block(1, [1,1], board))
+
+# print(check_number_present_in_adjacent_rows_columns(5, [1, 1], board))
